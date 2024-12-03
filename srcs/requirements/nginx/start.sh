af039025a8c30abb4ad14b9a860fc0516669ac34
Paul Großmann <git@paulgrossmann.de>
2024-12-03 18:27:44 +0100

    simplified gen of ssl certificate

--- srcs/requirements/nginx/start.sh
+++ srcs/requirements/nginx/start.sh
@@ -4,7 +4,6 @@ openssl req -x509 \
             -sha256 -days 356 \
             -nodes \
             -newkey rsa:2048 \
-            -subj "/CN=pgrossma.42.fr/C=EN/L=Heilbronn" \
             -keyout /etc/ssl/private/private.key -out /etc/ssl/certs/cert.crt
 
 nginx -g "daemon off;"
