94a1980cf7fa54a1b57f7230d293f6b4429ec7c9
Paul Großmann <git@paulgrossmann.de>
2024-12-03 18:34:48 +0100

    revert simplification of opensslgen

--- srcs/requirements/nginx/start.sh
+++ srcs/requirements/nginx/start.sh
@@ -4,6 +4,7 @@ openssl req -x509 \
             -sha256 -days 356 \
             -nodes \
             -newkey rsa:2048 \
+            -subj "/C=EN/L=Heilbronn" \
             -keyout /etc/ssl/private/private.key -out /etc/ssl/certs/cert.crt
 
 nginx -g "daemon off;"
